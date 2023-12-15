6151d547bb646b83a70af1bb37978b6034101aed
Lukaaaa <luka218393@gmail.com>
2023-12-16 00:16:40 +0100

    Add relations between tables

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -12,19 +12,21 @@ create table Books(
 	Id serial primary key not null,
 	Name varchar(55) not null,
 	Genre varchar(12) not null,
-	PublicationDate Timestamp
+	PublicationDate Timestamp,
+	LibraryId int references Libraries(Id)
 )
 alter table Books
 	add constraint NameISEmpty check(Name != '')
 alter table Books
 	add constraint BookGenre check (Genre in ('Reading', 'Artistic', 'Scientific', 'Biography', 'Professional'))
-	
+
 create table Authors(
 	Id serial primary key not null,
 	Name varchar(20) not null,
 	Surename varchar(20),
 	Gender varchar(1),
-	BirthDate Timestamp
+	BirthDate Timestamp,
+	Country int references Countries(Id)
 )
 alter table Authors
 	add constraint NameISEmpty check(Name != '')
@@ -32,7 +34,7 @@ alter table Authors
 	add constraint SurenameISEmpty check(Surename != '')
 alter table Authors
 	add constraint AuthorGender check (Gender in ('M', 'F', 'O', null))
-	
+
 create table Librarians(
 	Id serial primary key not null,
 	Name varchar(20) not null,
@@ -76,11 +78,19 @@ alter table Countries
 alter table Countries
 	add constraint PositiveAverageWage check(AverageWage > 0)
 	
+create table AuthorBook(
+	AuthorId int references Authors(Id) not null,
+	BookId int references Books(Id) not null,
+	Primary key (AuthorId, BookId)
+)
 	
-	
-	
-	
-	
+create table ReaderBook(
+	ReaderId int references Readers(Id) not null,
+	BookId int references Books(Id) not null,
+	BorrowDate TimeStamp,
+	BookReturned bool,
+	Primary key (ReaderId, BookId)
+)	
 	
 	
 	
